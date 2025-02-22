d5178603338035338754e53b15c90006c55f77c0
shivsundarbankar <shivsundarbankar@gmail.com>
2025-02-22 16:53:37 +0530

    war build

--- src/main/java/com/shivsundar/demo/JenkinsDemoApplication.java
+++ src/main/java/com/shivsundar/demo/JenkinsDemoApplication.java
@@ -2,13 +2,21 @@ package com.shivsundar.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class JenkinsDemoApplication {
+@ComponentScan(basePackages = "com.shivsundar")
+public class JenkinsDemoApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(JenkinsDemoApplication.class);
+	}
 
 	@GetMapping("/greetings/{name}")
 	public String greetings(@PathVariable String name) {
