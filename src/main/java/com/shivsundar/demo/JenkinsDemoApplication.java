c87ae920e684ce55f5eb5216b176ad9085fe4e7c
shivsundarbankar <shivsundarbankar@gmail.com>
2025-02-22 23:44:43 +0530

    build file name changed

--- src/main/java/com/shivsundar/demo/JenkinsDemoApplication.java
+++ src/main/java/com/shivsundar/demo/JenkinsDemoApplication.java
@@ -4,14 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@ComponentScan(basePackages = "com.shivsundar")
 public class JenkinsDemoApplication extends SpringBootServletInitializer {
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
